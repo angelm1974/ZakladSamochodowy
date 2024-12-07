e4b802015960fca87093d80514253d116cc3e645
angelm1974 <mariusz980@gmail.com>
2024-12-07 14:27:08 +0100

    Update Klient.cs
    nowy klient

--- ZakladSamochodowy/Zaklad/Klient.cs
+++ ZakladSamochodowy/Zaklad/Klient.cs
@@ -5,6 +5,13 @@ namespace Zaklad{
         public string Imie;
         public string Nazwisko;
         public string Telefon;
+    
+    public Klient(string imie, string nazwisko, string telefon)
+        {
+            Imie = imie;
+            Nazwisko = nazwisko;
+            Telefon = telefon;
+        }
 
     }
 }
